5a9ace4e6a321cc12b995750f58fd4d5bb9147af
Nick <nearlyheadllessnick@gmail.com>
2021-10-21 15:46:11 +0200

    Main layout

--- src/App.js
+++ src/App.js
@@ -34,7 +34,24 @@ function ListItem(props) {
     </li>
   )
 }
-
+function RecordCard(props) {
+  return (
+    <div className="RecordCard">
+      <div className="CheckboxContainer">
+        <input type="checkbox" id="statusCheckbox" name="statusCheckbox" value="finished" />
+      </div>
+      <div className="CardContainer">
+        <div className="CardHeader">
+          <p>{props.title}</p>
+          <svg width="20" height="6" viewBox="0 0 20 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M2.5 5.25C1.90326 5.25 1.33097 5.01295 0.90901 4.59099C0.487053 4.16903 0.25 3.59674 0.25 3C0.25 2.40326 0.487053 1.83097 0.90901 1.40901C1.33097 0.987053 1.90326 0.75 2.5 0.75C3.09674 0.75 3.66903 0.987053 4.09099 1.40901C4.51295 1.83097 4.75 2.40326 4.75 3C4.75 3.59674 4.51295 4.16903 4.09099 4.59099C3.66903 5.01295 3.09674 5.25 2.5 5.25ZM10 5.25C9.40326 5.25 8.83097 5.01295 8.40901 4.59099C7.98705 4.16903 7.75 3.59674 7.75 3C7.75 2.40326 7.98705 1.83097 8.40901 1.40901C8.83097 0.987053 9.40326 0.75 10 0.75C10.5967 0.75 11.169 0.987053 11.591 1.40901C12.0129 1.83097 12.25 2.40326 12.25 3C12.25 3.59674 12.0129 4.16903 11.591 4.59099C11.169 5.01295 10.5967 5.25 10 5.25ZM17.5 5.25C16.9033 5.25 16.331 5.01295 15.909 4.59099C15.4871 4.16903 15.25 3.59674 15.25 3C15.25 2.40326 15.4871 1.83097 15.909 1.40901C16.331 0.987053 16.9033 0.75 17.5 0.75C18.0967 0.75 18.669 0.987053 19.091 1.40901C19.5129 1.83097 19.75 2.40326 19.75 3C19.75 3.59674 19.5129 4.16903 19.091 4.59099C18.669 5.01295 18.0967 5.25 17.5 5.25Z" fill="#2C3E50" />
+          </svg>
+        </div>
+        <p>{props.text}</p>
+      </div>
+    </div>
+  )
+}
 function Header() {
   return (
     <div className="Header">
@@ -56,7 +73,15 @@ function Header() {
 function Main() {
   return (
     <div className="Main">
-      <p>texttexttexttexttexttexttexttexttext</p>
+      <p className="Label">Today</p>
+      <div className="RecordsList">
+        <RecordCard title="Card title"
+          text=" Some quick example text to build on the card title and make up the bulk of the card's content."></RecordCard>
+        <RecordCard title="Card title"
+          text=" Some quick example text to build on the card title and make up the bulk of the card's content."></RecordCard>
+        <RecordCard title="Card title"
+          text=" Some quick example text to build on the card title and make up the bulk of the card's content."></RecordCard>
+      </div>
     </div>
   );
 }
